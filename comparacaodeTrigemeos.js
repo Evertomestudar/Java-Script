0d553af19bb2802aca19cfbc5c02d3af082acc54
Evertom Alves <120690934+Evertomestudar@users.noreply.github.com>
2024-04-15 23:42:38 -0300

    Novo

---
+++ comparacaodeTrigemeos.js
@@ -0,0 +1,28 @@
+function comparacaodeTrigemeos(a,b){
+
+  let alice = 0;
+  let bob = 0;
+
+  for (let index = 0; index < a.length; index += 1 ){
+
+    if (a[index] > b[index] ){
+    
+      alice += 1;
+    
+    } 
+   else if(a[index] < b[index]){
+
+    bob += 1;
+
+   }
+
+
+    };
+
+    return [alice, bob];
+
+};
+
+console.log(comparacaodeTrigemeos([17, 28, 30], [99, 16, 8]));
+
+
---
+++ funcao.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+function soma(a, b){
+ return a + b;
+
+}
+
+console.log(soma(10,7));
+console.log(soma(20, 7));
